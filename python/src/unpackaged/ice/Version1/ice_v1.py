ad6c35b1188b7c0435dc2163859dabe8458d7866
Orkhan Hajiyev <46838261+ohajiyev@users.noreply.github.com>
2019-03-20 08:57:45 +0400

    Create ice_v1.py

---
+++ python/src/unpackaged/ice/Version1/ice_v1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 08:37:13 2019
+
+@author: Orkhan Hajiyev (gy17oh)
+
+The purpuse of the script is to implement the requirements of the 
+Assignment 2 of the Module GEOG5991M.
+
+White Star Line was selected as the project to satisfy the assignment's
+description. The link to the project's problem definition:
+    https://www.geog.leeds.ac.uk/courses/computing/
+    study/core-python-odl/assessment2/ice.html
+
+"""
