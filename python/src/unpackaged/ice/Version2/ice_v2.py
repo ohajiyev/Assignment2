846d0752b53caf85bac4778b1ca7e7cbcaab9a10
Orkhan Hajiyev <46838261+ohajiyev@users.noreply.github.com>
2019-03-29 10:20:24 +0400

    update

--- python/src/unpackaged/ice/Version2/ice_v2.py
+++ python/src/unpackaged/ice/Version2/ice_v2.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
 """
+Last updated on Mar 29, 2019
+
+@author: Orkhan Hajiyev (gy17oh)
 
 Title: Assignment 2. Module GEOG5991M
        White Star Line Project
 
 Version: 2.0
-The version was built to analyse MULTIPLE iceberg images 
-       
-Link to Github: https://github.com/ohajiyev/Assignment2
-    
-Last updated on Mar 27, 2019
-
-@author: Orkhan Hajiyev (gy17oh)
-
-Python version: 3.7 (Python 3.7.1 64-bit | Qt 5.9.6 | PyQt5 5.9.2 | Windows 10)
-
-The code is written in Spyder Version 3.3.2
-
-!!! Important note: some part of the code may be copied and modified from 
-!!! https://docs.python.org/3/ as a reference
+The version was built to analyse MULTIPLE iceberg images.
+It is possible to use single iceberg images as an input to run the code
 
 The purpuse of the script is to implement the requirements of the 
 Assignment 2 of the Module GEOG5991M.
@@ -31,6 +22,17 @@ description. The link to the project's problem definition:
 Copyright (c) 2019 Orkhan Hajiyev
 Lisence under MIT License
 License link: https://github.com/ohajiyev/Assignment2/blob/master/LICENSE.md
+       
+Link to Github: https://github.com/ohajiyev/Assignment2
+
+Instructions to run the script is written in 
+    
+Python version: 3.7 (Python 3.7.1 64-bit | Qt 5.9.6 | PyQt5 5.9.2 | Windows 10)
+
+The code was written in Spyder Version 3.3.2
+
+!!! Important note: some part of the code may be copied and modified from 
+!!! different sources which are explitely shown in the comments below
 
 """
 
@@ -52,21 +54,46 @@ import icebergstructure
 #==============================================================================
 # Function definitions
 
+def update_result_text_plot(ax_result, icebergs, iceberg_number):
+    """
+    The method update the resultant plot area appropriately with the text
+    obtained from Iceberg object
+    
+    Arguments required:
+        ax_result - subplot area of the resultant text
+        icebergs - list of Iceberg objects
+        iceberg_number - the number of the Iceberg object
+    """
+    # clear resultant subplot area before showing the text
+    ax_result.clear()
+    
+    # title for the result 
+    text_result = 'Results of image analysis of berg'
+    text_result += '\nHover over the icebergs to view characteristics'
+    ax_result.text(0, 115, text_result, 
+                 bbox={'facecolor': 'white', 'pad': 5})
+    ax_result.text(0, 285, str(icebergs[iceberg_number-1]), 
+                 bbox={'facecolor': 'white', 'pad': 5})
+    # turn off the resultant subplot area to keep only text
+    ax_result.axis('off')
+    # refresh plot area
+    ax_result.figure.canvas.draw_idle()
+
 def on_hover(event, icebergs, ice_data, ax_result, ice_count):
+    """
+    The method identify the mouse location over analysed image plot area and
+    call update method of the resultant plot area appropriately
+    
+    The following code is altered from the source 
+    https://stackoverflow.com/questions/7908636/possible-to-make-labels-appear-
+    when-hovering-over-a-point-in-matplotlib
+    """
     if event.inaxes is not None:
         if event.inaxes.title.get_text() == 'Analysed Icebergs':
-            ice_data_value = ice_data[int(round(event.ydata))][
+            iceberg_number = ice_data[int(round(event.ydata))][
                     int(round(event.xdata))]
-            if ice_data_value in range (1, ice_count+1):
-                ax_result.clear()
-                text_result = 'Results of image analysis of berg'
-                text_result += '\nHover over the icebergs to view characteristics'
-                ax_result.text(0, 115, text_result, 
-                             bbox={'facecolor': 'white', 'pad': 5})
-                ax_result.text(0, 285, str(icebergs[ice_data_value-1]), 
-                             bbox={'facecolor': 'white', 'pad': 5})
-                ax_result.axis('off')
-                plt.gcf().canvas.draw_idle()
+            if iceberg_number in range (1, ice_count+1):
+                update_result_text_plot(ax_result, icebergs, iceberg_number)
     
 def draw_result_multiple(ice_data, icebergs, \
                          radar_data_texture, lidar_data_height):
@@ -97,15 +124,19 @@ def draw_result_multiple(ice_data, icebergs, \
     value_description = {0: 'Sea level', 1: 'Iceberg pullable', \
                          2: 'Iceberg not pullable',}
     
-    # Create two plot area, one for image visualisation and the second will
-    # be used for showing the result text
+    # Create 4 plot areas, first pair at the top for input images visualisation 
+    # and the second pair for analysed image and the result text
     fig, ax = plt.subplots(2, 2, figsize=(8,8), sharey=True)
     
+    # Visualisation of input images
+    #==========================================================================
     ax[0, 0].imshow(radar_data_texture)
     ax[0, 0].set_title('Input Radar Image')
     ax[0, 1].imshow(lidar_data_height)
     ax[0, 1].set_title('Input Lidar Image')
-        
+    
+    # Visualisation of analysed images
+    #==========================================================================
     # Make a color map of fixed colors, blue for the sea and grey for 
     # the iceberg
     cmap = colors.ListedColormap(['blue', 'green', 'red'])
@@ -129,21 +160,21 @@ def draw_result_multiple(ice_data, icebergs, \
     
     ax[1, 0].set_title('Analysed Icebergs')
     
-    text_result = 'Results of image analysis of berg'
-    text_result += '\nHover over the icebergs to view characteristics'
-    
-    ax[1, 1].text(0, 115, text_result, bbox={'facecolor': 'white', 'pad': 5})
-    
-    ax[1, 1].text(0, 285, str(icebergs[0]), bbox={'facecolor': 'white', 
-      'pad': 5})
-    
-    ax[1, 1].axis('off')
-    
+    # Anotate the bergs on the analysed image
+    #==========================================================================
     ice_count = len(icebergs)
+    for berg in icebergs:
+        ice_ind = np.where(ice_data == berg.ice_no)
+        ax[1, 0].annotate(str(berg.ice_no), xy=(ice_ind[1][0], 
+          ice_ind[0][0]-10), bbox=dict(boxstyle="round, pad=0.1", fc="w"))
+        
+    # Visualisation of the result text
+    #==========================================================================
+    update_result_text_plot(ax[1, 1], icebergs, 1)
     
-    plt.show()
-    
-    cid = fig.canvas.mpl_connect('motion_notify_event', lambda event: 
+    # Activation of hovering function to view result text of analysed bergs
+    #==========================================================================
+    fig.canvas.mpl_connect('motion_notify_event', lambda event: 
         on_hover(event, icebergs, ice_data, ax[1, 1], ice_count))
     
     plt.show()
@@ -217,51 +248,60 @@ def iceberg_identification(radar_data_texture):
 # End of Function definitions
 #==============================================================================
 
+def main():
+    #==========================================================================
+    # Create variables. 
+    
+    # empty list of data which contains texture of objects read from radar file 
+    radar_data_texture = []
+    # empty list of data which contains height of objects read from lidar file 
+    lidar_data_height = [] 
+    # empty list of Iceberg objects
+    icebergs = [] 
+    
+    # Uncommenting of single input images and commenting of multiple berg 
+    # images allow to test the code based on single iceberg images
+    # Define input file paths for single bergs
+    #lidar_data_file_path = 'input/white1.lidar'
+    #radar_data_file_path = 'input/white1.radar'
+    
+    # Define input file paths for multiple bergs
+    lidar_data_file_path = 'input/white2.lidar'
+    radar_data_file_path = 'input/white2.radar'
+    
+    # Define output file path
+    output_file_path = 'output/results.txt'
+    
+    # End of Create variables
+    #==========================================================================
+    
+    
+    # Main part of the code
+    #==========================================================================
+    
+    # Read input files and assign texture and height info to the variables
+    lidar_data_height = read_file(lidar_data_file_path)
+    radar_data_texture = read_file(radar_data_file_path)
+    
+    # Identify the mupltiple icebergs and count from the radar image
+    ice_data, ice_count = iceberg_identification(radar_data_texture)
+    
+    # Create the Iceberg objects from the input  image files
+    for ice_no in range(1, ice_count + 1):
+        icebergs.append(icebergstructure.Iceberg(ice_data, lidar_data_height,
+                                                 ice_no))
+    
+    # Draw input images and result (both image and text) in the canvas
+    # and activate hovering fucntion to view the resultant text of the 
+    # specific iceberg
+    draw_result_multiple(ice_data, icebergs, radar_data_texture, 
+                         lidar_data_height)
+    
+    # Write the results of the analysis to text file
+    write_file(output_file_path, icebergs)
+    
+    # End of Main part of the code
+    #==========================================================================
 
-#==============================================================================
-# Create variables. 
-
-radar_data_texture = [] # empty list of data which contains texture of objects
-                         # read from radar file   
-lidar_data_height = [] # empty list of data which contains height of objects
-                        # read from lidar file 
-icebergs = [] # empty list of Iceberg objects
-
-# Define input file paths for single bergs
-#lidar_data_file_path = 'input/white1.lidar'
-#radar_data_file_path = 'input/white1.radar'
-
-# Define input file paths for multiple bergs
-lidar_data_file_path = 'input/white2.lidar'
-radar_data_file_path = 'input/white2.radar'
-
-# Define output file path
-output_file_path = 'output/results.txt'
-
-# End of Create variables
-#==============================================================================
-
-
-# Main part of the code
-#==============================================================================
-
-# Read input files and assign texture and height info to the variables
-lidar_data_height = read_file(lidar_data_file_path)
-radar_data_texture = read_file(radar_data_file_path)
-
-# Identify the mupltiple icebergs from the radar image
-ice_data, ice_count = iceberg_identification(radar_data_texture)
-
-# Create the Iceberg object from the input  image files
-for ice_no in range(1, ice_count + 1):
-    icebergs.append(icebergstructure.Iceberg(ice_data, lidar_data_height, \
-                                             ice_no))
-
-# Draw result in the canvas
-draw_result_multiple(ice_data, icebergs, radar_data_texture, lidar_data_height)
-
-# Write the results of the analysis to text file
-write_file(output_file_path, icebergs)
-
-# End of Main part of the code
-#==============================================================================
+if __name__ == '__main__':
+    main()
